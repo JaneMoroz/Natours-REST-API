9e5bf32316b3ee06e909d764fc6b6da8258f3238
Jane Moroz <Moroz.jane@yahoo.com>
2022-02-08 13:06:32 +0300

    Created And Tested the Model, Added Data To Database

--- server.js
+++ server.js
@@ -22,6 +22,44 @@ async function dbConnect() {
   console.log('DB connection successful!');
 }
 
+// Scheme
+const tourScheme = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// Model
+const Tour = mongoose.model('Tour', tourScheme);
+
+// const testTour = new Tour({
+//   name: 'The Forest Hiker',
+//   rating: 4.7,
+//   price: 497,
+// });
+
+const testTour = new Tour({
+  name: 'The Lake Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => console.log('Error 💥', err));
+
 ////////////////////////////////////////////////////////////////
 // Server
 
