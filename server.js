6fabea8ff32c2bd3af57d2b2477860048ca48729
Jane Moroz <Moroz.jane@yahoo.com>
2022-02-08 12:33:52 +0300

    Connected Database

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 ////////////////////////////////////////////////////////////////
+// Mongoose
+const mongoose = require('mongoose');
+
+////////////////////////////////////////////////////////////////
 // Environment variables
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.env.development.local' });
 
 ////////////////////////////////////////////////////////////////
 // Get app
 const app = require('./app');
+
+////////////////////////////////////////////////////////////////
+// Connect to DB
+const DB = process.env.DATABASE;
+
+dbConnect().catch((err) => console.log(err));
+
+async function dbConnect() {
+  const con = await mongoose.connect(DB);
+  console.log('DB connection successful!');
+}
 
 ////////////////////////////////////////////////////////////////
 // Server
