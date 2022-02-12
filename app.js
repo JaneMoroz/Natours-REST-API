243c79e301328a6ac3a028f5ea39aaadb9eb4fbd
Jane Moroz <Moroz.jane@yahoo.com>
2022-02-12 16:20:39 +0300

    Added Ability to Handle Unhandled Routes

--- app.js
+++ app.js
@@ -17,17 +17,8 @@ if (process.env.NODE_ENV === 'development') {
 // Express middleware
 app.use(express.json());
 
-// // 3-rd party middleware
-// app.use(morgan('dev'));
-
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
-
-// Create our own middleware
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 😏');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -39,5 +30,13 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handling Unhandled Routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
