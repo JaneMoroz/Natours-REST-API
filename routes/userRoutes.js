87d88dd1783bc0fc4558bcfa61db2242dd8d227a
Jane Moroz <Moroz.jane@yahoo.com>
2022-02-14 13:54:29 +0300

    Added "signup" Route And Handler

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 ////////////////////////////////////////////////////////////////
 // Routing
 
 const router = express.Router();
 
+router.post('/signup', authController.signup);
+
 router
   .route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
+
 router
   .route('/:id')
   .get(userController.getUser)
