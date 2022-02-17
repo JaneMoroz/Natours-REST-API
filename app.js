5deb4e3584e0b200565e27ef76db1a67a98d9e80
Jane Moroz <Moroz.jane@yahoo.com>
2022-02-17 20:46:53 +0300

    Added Reviews Routes

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -70,6 +72,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // Handling Unhandled Routes
 app.all('*', (req, res, next) => {
