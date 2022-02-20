16df8cdd1c861da32a2e8b5ab62fe49c2abae742
Jane Moroz <Moroz.jane@yahoo.com>
2022-02-20 19:30:39 +0300

    Added Ability to Find Tours Within Radius and Calculate Distances

--- models/tourModel.js
+++ models/tourModel.js
@@ -138,6 +138,7 @@ const tourSchema = new mongoose.Schema(
 // Work with indexes
 tourSchema.index({ price: 1, ratingsAverage: -1 });
 tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: '2dsphere' });
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
@@ -187,10 +188,10 @@ tourSchema.post(/^find/, function (docs, next) {
 });
 
 // Aggregation Middleware
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  next();
-});
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//   next();
+// });
 
 // Model
 const Tour = mongoose.model('Tour', tourSchema);
