6e27e74a82ae52a395806ee7818b36d565f40351
Jane Moroz <Moroz.jane@yahoo.com>
2022-02-20 17:05:37 +0300

    Added Indexes

--- models/tourModel.js
+++ models/tourModel.js
@@ -134,6 +134,10 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+// Work with indexes
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
