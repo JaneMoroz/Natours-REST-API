5c19557138d4e3a21c83825194401641139efe79
Jane Moroz <Moroz.jane@yahoo.com>
2022-03-13 18:36:45 +0300

    Built the Tour Page & Fixed Root Bug

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -5,17 +5,23 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   // 1. Get Data
   const tours = await Tour.find();
   // 2. Build Template
-
   // 3. Render Template Using Data
-
   res.status(200).render('overview', {
     title: 'All Tours',
     tours,
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1. Get Data for the Requested Tour (incls reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    select: 'review rating user',
   });
-};
+  // 2. Build Template
+  // 3. Render Remplate Using the Data
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour,
+  });
+});
