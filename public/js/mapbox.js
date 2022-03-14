bcd895792a77e7ba8e5df059140f6c7aad66153f
Jane Moroz <Moroz.jane@yahoo.com>
2022-03-14 18:13:03 +0300

    Mapbox

---
+++ public/js/mapbox.js
@@ -0,0 +1,50 @@
+console.log('hELLO');
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+console.log(locations);
+
+// MapBox
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiamFuZW1vcm96MjUiLCJhIjoiY2wwcW95d2xpMDNuNDNmcHEwMmw3cWx2cCJ9.nRIPZy1xa8L7VUTnomaf3w';
+
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/jonasschmedtmann/cjvi9q8jd04mi1cpgmg7ev3dy',
+  scrollZoom: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
